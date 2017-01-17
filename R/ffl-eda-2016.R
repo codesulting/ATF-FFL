f03d220a51b07d268d7f0e15c3823a2d4692e952
Pete Deevakul <editor@mozzarella.website>
2017-01-17 03:52:50 -0500

    plots by region and month

--- R/ffl-eda-2016.R
+++ R/ffl-eda-2016.R
@@ -29,20 +29,18 @@ summary(f16$LicCount)
 # A look at license count by state, filled by region
 ggplot(f16, aes(reorder(PremiseStateFull, LicCount), fill = LicCount)) + 
   geom_bar() +
-  scale_fill_gradient2(low = "deepskyblue2", mid = "antiquewhite2", high = muted("firebrick4"),
+  scale_fill_gradient2(low = "deepskyblue2", mid = "antiquewhite2", high = muted("firebrick3"),
                        midpoint = 25220) +
-  scale_y_discrete(limits = c(0, 80000)) +
+  scale_y_discrete(limits = c(0, 5000, 10000, 20000, 40000, 80000)) +
   theme_minimal(base_size = 13.75, base_family = "GillSans") +
   theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
         axis.title.x = element_text(family = "Times", face = "italic", size = 12,
                                     margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
-        axis.text.x = element_text(size = 13.25),
+        axis.text.x = element_text(size = 12.75, angle = 30, hjust = 1, vjust = 1),
         panel.grid.major = element_line(color = "gray96")) +
   labs(title = "2016: Federal Firearms Licenses by State",
        y = "number of licenses", x = "", fill = "") +
   coord_flip()
-
-
 
 # same variables; points instead of bar
 ggplot(f16, aes(PremiseStateFull, LicCount, color = LicCount)) + 
@@ -59,37 +57,32 @@ ggplot(f16, aes(PremiseStateFull, LicCount, color = LicCount)) +
   coord_flip()
 
 
+# facet plots by region -------------------------------------------------------
 
-# purely by Region
-f16$Region <- factor(f16$Region)
+f16 %>% group_by(Region)
 
-ggplot(f16, aes(Region, fill = Region)) + 
-  geom_bar(color = "gray82", position = "dodge") +
-  scale_fill_manual(values = region.pal) +
-  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
-  theme(plot.margin = unit(c(1, 1, 1, 1), "cm")) +
-  labs(title = "2016: Federal Firearms License by State & Region",
-       x = "", y = "") +
-  coord_flip()
+ggplot(f16, aes(LicCount, PremiseState, color = LicCount)) +
+  geom_point(size = 3) +
+  facet_grid(. ~ Region) +
+  scale_color_gradient(low = "deepskyblue3", high = muted("firebrick4"),
+                        guide = FALSE) +
+  theme_gray(base_size = 12, base_family = "GillSans") +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        axis.title = element_text(family = "Times", face = "italic")) +
+  labs(title = "2016: Federal Firearms Licenses by State ~ Region",
+       x = "number of licenses", y = "premise state")
 
-
-
-# group by region
-FFL16_Regions <- f16 %>%
-  group_by(Region, month) %>%
-  count(PremiseState, sort = T)
-
-colnames(FFL16_Regions) <- c("Region", "month", "PremiseState", "LicCount")
-str(FFL16_Regions)
-var(FFL16_Regions$LicCount)
-
-ggplot(FFL16_Regions) +
-  geom_point(mapping = aes(LicCount, PremiseState, color = Region)) +
-  scale_fill_manual(values = region.pal) +
-  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
-  theme(plot.margin = unit(c(1, 1, 1, 1), "cm")) +
-  labs(title = "2016: Federal Firearms License by State & Region", x = "", y = "")
-
+# by region, without state
+ggplot(f16, aes(LicCount, color = LicCount)) +
+  geom_bar() +
+  facet_grid(. ~ Region) +
+  scale_color_gradient(low = "deepskyblue3", high = muted("firebrick4"),
+                       guide = FALSE) +
+  theme_gray(base_size = 12, base_family = "GillSans") +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        axis.title = element_text(family = "Times", face = "italic")) +
+  labs(title = "2016: Federal Firearms Licenses by Region",
+       x = "number of licenses", y = "premise state")
 
 # Was there much variance from month to month? --------------------------------
 
@@ -98,25 +91,67 @@ month_pal <- c("deepskyblue4", "deepskyblue3", "lightblue2", "bisque2", "bisque3
                "firebrick2", "firebrick3", "firebrick4", "lightblue4", "lightblue3")
 
 # license holder count by state, stack by month
-ggplot(f16, aes(PremiseState, fill = month)) + 
+ggplot(f16, aes(PremiseStateFull, fill = month)) + 
   geom_bar() +
   scale_fill_manual(values = month_pal) +
-  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
-  theme(plot.margin = unit(c(1, 1, 1, 1), "cm")) +
-  labs(title = "2016: Federal Firearms Licenses per month, by State/Territory", x = "", y = "") +
+  theme_minimal(base_size = 14, base_family = "GillSans") +
+  theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
+        axis.title = element_text(family = "Times", face = "italic", size = 12,
+                                  margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))) +
+  labs(title = "2016: Federal Firearms Licenses per month, by State/Territory", 
+       x = "", y = "number of licenses") +
+  coord_flip()
+
+# facet by month
+ggplot(f16, aes(PremiseStateFull, fill = LicCount)) + 
+  geom_bar() +
+  facet_grid(. ~ month) +
+  scale_fill_gradient2(low = "deepskyblue2", mid = "antiquewhite2", high = muted("firebrick3"),
+                        midpoint = 25220) +
+  scale_y_continuous(limits = c(0, 8000), breaks = c(2000, 4000, 6000, 8000)) +
+  theme_gray(base_size = 14, base_family = "GillSans") +
+  theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
+        axis.title = element_text(family = "Times", face = "italic", size = 12,
+                                  margin = unit(c(0.5, 0, 0, 0), "cm")),
+        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+        legend.title = element_text(family = "Times", face = "italic", size = 12)) +
+  labs(title = "2016: Federal Firearms Licenses per month, by State/Territory", 
+       x = "", y = "number of licenses", fill = "annual total") +
   coord_flip()
 
 # There was not much variance from month to month.
 # this is likely due to licenses not expiring.
 
+# Summary by Region -----------------------------------------------------------
 
-# Are there multiples of the same license holder each month? ------------------
-f16_unique <- f16[!duplicated(f16$`License Name`), ]
+f16_regions <- f16 %>%
+  group_by(Region) %>%
+  count(PremiseState)
 
-ggplot(f16_unique, aes(PremiseState)) + 
-  theme_minimal(base_size = 12, base_family = "FranklinGothicSSK") +
-  geom_bar() +
-  coord_flip()
+colnames(f16_regions) <- c("Region", "PremiseState", "LicCount")
+
+# there has to be a more efficient way to do this
+region01 <- f16_regions %>%  filter(Region == 1)
+region03 <- f16_regions %>% filter(Region == 3)
+region04 <- f16_regions %>% filter(Region == 4)
+region05 <- f16_regions %>% filter(Region == 5)
+region06 <- f16_regions %>% filter(Region == 6)
+region08 <- f16_regions %>% filter(Region == 8)
+region09 <- f16_regions %>% filter(Region == 9)
+
+f16_regions2 <- f16 %>%
+  group_by(Region) %>%
+  count(PremiseState)
+
+# facet plots by firearms type ------------------------------------------------
+getwd()
+type <- read.csv("data/type.tsv", sep = "\t")
+
+levels(as.factor(f16$Type))
+f16$Type <- as.numeric(f16$Type)
+
+f16_typ <- f16 %>%
+  gather()
 
 
 # What percentage of the population has a Federal Firearms License? -----------
