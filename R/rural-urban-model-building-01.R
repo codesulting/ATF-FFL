8db84045df0f6148d76e9847441bc6684eb025c7
Pete Deevakul <editor@mozzarella.website>
2017-02-24 01:42:44 -0500

    model FFLs ~ Rural Percentages and Land Area
    plot histogram of residuals, per capita FFL counts, and model itself

---
+++ R/rural-urban-model-building-01.R
@@ -0,0 +1,65 @@
+# ATF - FFL
+# Rural-Urban Proportions Model Building - 01
+# https://www.census.gov/geo/reference/ua/urban-rural-2010.html
+# https://www.census.gov/geo/reference/ua/ualists_layout.html
+
+# load data -------------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(broom)
+library(MASS)
+
+# State Level Rural-Urban Proportions
+# rural.urban <- read.csv("~/GitHub/ATF-FFL/data/PctUrbanRural_State.csv")
+
+# per Capita FFLs 2016
+# perCap16 <- read.csv("~/GitHub/ATF-FFL/data/ffl-2016-perCapita.csv")
+
+# load plot themes
+source("~/GitHub/ATF-FFL/R/00-pd-themes.R")
+
+# cleansed dataframe with FFL & Rural-Urban data
+ffl.16 <- read.csv("~/GitHub/ATF-FFL/data/rural-urban-compact.csv")
+str(ffl.16)
+names(ffl.16)
+
+rownames(ffl.16) <- ffl.16$NAME
+
+# Model Building --------------------------------------------------------------
+
+# m01 - Do Rural and Urban Cluster Population Percentages and Land Area 
+# have a relationship with per capita FFL counts? 
+
+rural.urban.01 <- lm(perCapitaFFL ~ 
+                       POPPCT_RURAL + POPPCT_UC + 
+                       AREAPCT_RURAL + AREAPCT_UC + 
+                       AREA_RURAL + AREA_UC, data = ffl.16)
+
+summary(rural.urban.01)
+tidy(rural.urban.01)
+glance(rural.urban.01)[, c(1:5, 8, 10)]
+
+ru.01.fit <- augment(rural.urban.01)
+
+# histogram of residuals
+ggplot(ru.01.fit, aes(.resid)) +
+  geom_histogram(binwidth = 8, color = "black", fill = "white") +
+  pd.theme +
+  theme(axis.line = element_line(color = "black")) +
+  labs(title = "Distribution of Residuals - lm `rural.urban.01`")
+
+# histogram of per capita FFL counts
+ggplot(ffl.16, aes(perCapitaFFL)) +
+  geom_histogram(binwidth = 8, color = "black", fill = "white") +
+  pd.theme +
+  theme(axis.line = element_line(color = "black")) +
+  labs(title = "Distribution of FFLs per capita, by state")
+
+hist(ru.01.fit$.resid)
+
+
+
+
+
