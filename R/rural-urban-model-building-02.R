ae898913356698b243d5570479676052e3127e3d
Pete Deevakul <editor@mozzarella.website>
2017-02-25 01:26:01 -0500

    plots for robust regression 01

--- R/rural-urban-model-building-02.R
+++ R/rural-urban-model-building-02.R
@@ -59,8 +59,71 @@ huber01[1:12, ]
 rr.huber01 <- augment(rr01) %>%
   left_join(huber01) %>%
   arrange(weight) %>%
-  mutate(weighted.resid = .resid * weight)
-
+  mutate(weighted.resid = .resid * weight,
+         weighted.fit = .fitted + weighted.resid) %>%
+  
 rr.huber01
 
+# distribution of weighted residuals
+ggplot(rr.huber01, aes(weighted.resid)) +
+  geom_histogram(binwidth = 8, color = "black", fill = NA) +
+  labs(x = "weighted residuals") +
+  pd.scatter
+
+# compute distances between observed and weighted fit
+# rr.huber01 <- rr.huber01 %>% 
+#   mutate(distance = perCapitaFFL - weighted.fit)
+
+# plot weighted fit values
+ggplot(rr.huber01, aes(POPPCT_UC, perCapitaFFL, label = .rownames)) + 
+  geom_point(size = 1.5) +
+  geom_text(aes(POPPCT_UC, perCapitaFFL), 
+            size = 3, hjust = 1.15, vjust = -0.65, 
+            check_overlap = T, family = "GillSans") +
+  geom_point(aes(POPPCT_UC, weighted.fit), color = "firebrick3", 
+             shape = 23, size = 4, alpha = 0.8, data = rr.huber01) +
+  geom_errorbar(aes(x = POPPCT_UC, 
+                    ymin = weighted.fit, 
+                    ymax = perCapitaFFL), 
+                    linetype = "dotted") +
+  labs(x = "percentage of population living in Urban Clusters",
+       title = "Robust Model 01: Observed vs. Fitted Values, Huber Weighting") +
+  pd.scatter
+
+rr01.coef <- tidy(rr01)
+summary(rr.huber01$weighted.fit)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 3.687  20.070  26.100  30.490  38.780  81.820
+
+# plot fitted with regression line, fill mapped to fit value
+ggplot(rr.huber01, aes(POPPCT_UC, weighted.fit, 
+                       fill = weighted.fit, 
+                       label = .rownames)) + 
+  geom_point(color = "firebrick3", shape = 23, size = 3.5, 
+             alpha = 0.8, data = rr.huber01) +
+  geom_text(aes(POPPCT_UC, weighted.fit), 
+            size = 3, hjust = 1.15, vjust = -0.65, 
+            check_overlap = T, family = "GillSans") +
+  geom_smooth(method = "lm", se = F, color = "deepskyblue4", 
+              size = 0.5, linetype = "dashed") +
+  scale_fill_gradient2(low = "deepskyblue4", 
+                       mid = "antiquewhite1", 
+                       high = "firebrick3",
+                       midpoint = 40, guide = F) +
+  labs(x = "percentage of population living in Urban Clusters",
+       title = "Robust Model 01: Weighted Fit Values (Huber)") +
+  pd.scatter
+
+# plot fitted with regression line, no color fill
+ggplot(rr.huber01, aes(POPPCT_UC, weighted.fit, label = .rownames)) + 
+  geom_point(color = "firebrick3", shape = 23, size = 3.5, 
+             alpha = 0.8, data = rr.huber01) +
+  geom_text(aes(POPPCT_UC, weighted.fit), 
+            size = 3, hjust = 1.15, vjust = -0.65, 
+            check_overlap = T, family = "GillSans") +
+  geom_smooth(method = "lm", se = F, color = "deepskyblue4", 
+              size = 0.5, linetype = "dashed") +
+  labs(x = "percentage of population living in Urban Clusters",
+       title = "Robust Model 01: Weighted Fit Values (Huber)") +
+  pd.scatter
 
