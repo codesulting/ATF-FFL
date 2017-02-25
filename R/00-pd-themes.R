ee1d34f757e0a209e2952d6ca3df45994d1c65bb
Pete Deevakul <editor@mozzarella.website>
2017-02-25 00:47:16 -0500

    modify minimal theme for scatterplots with axis lines

--- R/00-pd-themes.R
+++ R/00-pd-themes.R
@@ -10,6 +10,13 @@ pd.theme <- theme_minimal(base_size = 12, base_family = "GillSans") +
         axis.title.x = element_text(margin = margin(20, 0, 0, 0)),
         axis.title.y = element_text(margin = margin(0, 20, 0, 0)))
 
+pd.scatter <- theme_minimal(base_size = 12, base_family = "GillSans") +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        axis.title = element_text(family = "Times", face = "italic", size = 12),
+        axis.title.x = element_text(margin = margin(20, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 20, 0, 0)),
+        axis.line = element_line(color = "black"))
+
 pd.classic <- theme_classic(base_size = 12, base_family = "GillSans") +
   theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
         axis.title = element_text(family = "Times", face = "italic", size = 12),
