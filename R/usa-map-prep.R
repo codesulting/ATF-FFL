d2afc1633f9618cecd8bfef310c4c9778573b7f4
Pete Deevakul <editor@mozzarella.website>
2017-02-27 16:26:23 -0500

    dedicated script for loading map data by state and county

--- R/usa-map-prep.R
+++ R/usa-map-prep.R
@@ -2,11 +2,14 @@
 
 library(maps)
 library(mapproj)
+library(maptools)
+library(sp)
+library(rgdal)
+library(tigris)
 library(fiftystater)
 library(rgeos)
-library(rgdal)
-library(maptools)
 library(dplyr)
+library(ggplot2)
 
 # Function: Capwords ----------------------------------------------------------
 
@@ -47,8 +50,29 @@ fifty_states$NAME <- capwords(fifty_states$NAME)
 ggplot(fifty_states, aes(lon, lat, group = group)) +
   geom_path() + coord_map("polyconic")
 
+# USA by county  --------------------------------------------------------------
 
+us.county <- map_data("county")
 
+colnames(us.county)[5:6] <- c("NAME", "County")
+us.county$NAME <- capwords(us.county$NAME)
+us.county$County <- capwords(us.county$County)
 
+ggplot(us.county, aes(long, lat, group = group)) +
+  geom_path() + coord_map("polyconic")
 
+# USA by county shp -----------------------------------------------------------
 
+# Download county shape file.
+us.map <- tigris::counties(cb = TRUE, year = 2015)
+
+# Remove Alaska(2), Hawaii(15), Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60)
+#  Mariana Islands (69), Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
+us.map <- us.map[!us.map$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
+                                        "64", "68", "70", "74"),]
+# Make sure other outling islands are removed.
+us.map <- us.map[!us.map$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
+                                        "95", "79"),]
+# Produce map
+county_map <- fortify(us.map, region = "NAME")
+colnames(county_map)[6] <- "COUNTYNAME"
