4b8fe436569c276c03923719f82121194628a7d4
Pete Deevakul <editor@mozzarella.website>
2017-03-19 03:32:55 -0400

    remove county level map data

--- R/usa-map-prep.R
+++ R/usa-map-prep.R
@@ -4,10 +4,7 @@ library(maps)
 library(mapproj)
 library(maptools)
 library(sp)
-library(rgdal)
-library(tigris)
 library(fiftystater)
-library(rgeos)
 library(dplyr)
 library(ggplot2)
 
@@ -21,22 +18,7 @@ capwords <- function(s, strict = FALSE) {
   sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
 }
 
-# USA Map preparation ---------------------------------------------------------
-
-# load map data for US
-usa <- map_data("state")
-
-# match variable names in FFL data to merge with US map
-colnames(usa) <- c("lon", "lat", "group", "order", "NAME", "subregion")
-
-# capitalize state names for binding
-usa$NAME <- capwords(usa$NAME)
-
-# test map out
-ggplot(usa, aes(lon, lat, group = group)) +
-  geom_path() + coord_map("polyconic") 
-
-# Alaska and Hawaii -----------------------------------------------------------
+# Alaska and Hawaii Maps-----------------------------------------------------------
 
 # load fifty states data
 data("fifty_states")
@@ -60,19 +42,3 @@ us.county$County <- capwords(us.county$County)
 
 ggplot(us.county, aes(long, lat, group = group)) +
   geom_path() + coord_map("polyconic")
-
-# USA by county shp -----------------------------------------------------------
-
-# Download county shape file.
-us.map <- tigris::counties(cb = TRUE, year = 2015)
-
-# Remove Alaska(2), Hawaii(15), Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60)
-#  Mariana Islands (69), Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
-us.map <- us.map[!us.map$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
-                                        "64", "68", "70", "74"),]
-# Make sure other outling islands are removed.
-us.map <- us.map[!us.map$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
-                                        "95", "79"),]
-# Produce map
-county_map <- fortify(us.map, region = "NAME")
-colnames(county_map)[6] <- "COUNTYNAME"
