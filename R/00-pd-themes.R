5a143e1a8dba9c52eba64ffba47225dc59c4166c
Pete Deevakul <editor@mozzarella.website>
2017-03-21 03:06:08 -0400

    create theme for facet plots

--- R/00-pd-themes.R
+++ R/00-pd-themes.R
@@ -22,3 +22,11 @@ pd.classic <- theme_classic(base_size = 12, base_family = "GillSans") +
         axis.title = element_text(family = "Times", face = "italic", size = 12),
         axis.title.x = element_text(margin = margin(20, 0, 0, 0)),
         axis.title.y = element_text(margin = margin(0, 20, 0, 0)))
+
+pd.facet <- theme_minimal(base_size = 12, base_family = "GillSans") +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        axis.title = element_text(family = "Times", face = "italic", size = 12),
+        axis.title.x = element_text(margin = margin(20, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 20, 0, 0)),
+        panel.background = element_rect(fill = NA, color = "black"),
+        strip.background = element_rect(fill = NA, color = "black"))
