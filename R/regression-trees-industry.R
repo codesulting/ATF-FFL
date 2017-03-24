8fff448c20d188037cec16cb7fccc008e4009ac8
Pete Deevakul <editor@mozzarella.website>
2017-03-24 03:59:35 -0400

    remove ggdendro library, rename variables

--- R/regression-trees-industry.R
+++ R/regression-trees-industry.R
@@ -11,7 +11,7 @@ library(rpart)
 library(rpart.plot)
 library(tree)
 library(ggplot2)
-library(ggdendro)
+library(ggendro)
 
 # load themes and functions
 source("~/GitHub/ATF-FFL/R/00-pd-themes.R")
@@ -26,6 +26,7 @@ str(industry.perCapita)
 
 # `rpart` model
 industry.tree.a <- rpart(perCapitaFFL ~ ., data = industry.perCapita)
+
 rpart.plot(industry.tree.a, type = 1, extra = 1,
            digits = 4, cex = 0.85, 
            split.family = "GillSans", split.cex = 1.1,
@@ -34,14 +35,17 @@ rpart.plot(industry.tree.a, type = 1, extra = 1,
 
 print(industry.tree.a)
 
+
+# Examing Split Variable Distributions ----------------------------------------
+
 # distribtion of Agriculture
-summary(industry.perCapita$agriculturePC)
+summary(industry.perCapita$agriculture)
 #    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #   162.3   517.4  1020.0  1299.0  1655.0  6497.0
 
 # distribtion of tree-split varibles
 industry.perCapita %>%
-  select(agriculturePC, manufacturingPC, perCapitaFFL) %>%
+  select(agriculture, manufacturing, perCapitaFFL) %>%
   mutate(NAME = rownames(industry.perCapita)) %>%
   gather("industry", "pop.per.100k", 1:2) %>%
   ggplot(aes(x = pop.per.100k)) +
@@ -53,9 +57,9 @@ industry.perCapita %>%
   labs(x = "population per 100k",
        title = "Distribution of Population by Industry: Tree Split Variables")
 
-# distribtion of tree-split varibles
+# boxplot of tree-split varibles
 industry.perCapita %>%
-  select(agriculturePC, manufacturingPC, perCapitaFFL) %>%
+  select(agriculture, manufacturing, perCapitaFFL) %>%
   mutate(NAME = rownames(industry.perCapita)) %>%
   gather("industry", "pop.per.100k", 1:2) %>%
   ggplot(aes(x = industry, y = pop.per.100k)) +
@@ -63,24 +67,27 @@ industry.perCapita %>%
   labs(y = "population per 100k", x = "industry",
        title = "Distribution of Population by Industry: Tree Split Variables")
 
-# scatter of tree-split varibles
+# Industry: Decision Tree Splits ----------------------------------------------
 
+# scatterplot of tree-split varibles
 industry.perCapita$.rownames <- rownames(industry.perCapita)
 
-ggplot(industry.perCapita, aes(agriculturePC, manufacturingPC, label = .rownames)) +
+ggplot(industry.perCapita, aes(agriculture, manufacturing, label = .rownames)) +
+  geom_segment(x = 2287, xend = 2287, y = 0, yend = 10000,
+               linetype = "dashed", color = "red3", size = 0.25) +
+  geom_segment(x = 1269, xend = 1269, y = 0, yend = 10000, 
+               linetype = "dashed", color = "red3", size = 0.25) +
+  geom_segment(x = -1000, xend = 1269, y = 5250, yend = 5250, 
+               linetype = "dashed", color = "red3", size = 0.10) +
   geom_point(aes(size = perCapitaFFL*8, color = perCapitaFFL)) +
   scale_color_gradient2(low = "deepskyblue4",
                         mid = "antiquewhite2",
                         high = "firebrick4", midpoint = 52) +
-  geom_segment(x = 2287, xend = 2287, y = 0, yend = 10000,
-               linetype = "dotted", color = "red3", size = 0.05) +
-  geom_segment(x = 1269, xend = 1269, y = 0, yend = 10000, 
-               linetype = "dotted", color = "red3", size = 0.05) +
-  geom_segment(x = 0, xend = 1269, y = 5290, yend = 5290, 
-               linetype = "dotted", color = "red3", size = 0.05) +
-  geom_text(aes(agriculturePC, manufacturingPC, label = .rownames),
-            size = 3, hjust = -0.01, vjust = -0.55, 
+  scale_size(name = "per capita FFLs", range = c(1, 18)) +
+  geom_text(aes(agriculture, manufacturing, label = .rownames),
+            size = 3, hjust = 0.9, vjust = -0.55, 
             check_overlap = T, family = "GillSans", data = industry.perCapita) +
+  expand_limits(x = c(-50, 7000)) +
   pd.facet +
   theme(legend.position = "none",
         legend.title = element_text(size = 10),
@@ -88,3 +95,31 @@ ggplot(industry.perCapita, aes(agriculturePC, manufacturingPC, label = .rownames
   labs(x = "Agriculture, Hunting & Fishing, Mining workforce per 100k", 
        y = "Manufacturing workforce per 100k",
        color = "per capita FFLs")
+
+# high resolution version
+ggplot(industry.perCapita, aes(agriculture, manufacturing, label = .rownames)) +
+  geom_segment(x = 2287, xend = 2287, y = 0, yend = 10000,
+               linetype = "dashed", color = "red3", size = 1) +
+  geom_segment(x = 1269, xend = 1269, y = 0, yend = 10000, 
+               linetype = "dashed", color = "red3", size = 1) +
+  geom_segment(x = -1000, xend = 1269, y = 5250, yend = 5250, 
+               linetype = "dashed", color = "red3", size = 1) +
+  geom_point(aes(size = perCapitaFFL*8, color = perCapitaFFL)) +
+  scale_color_gradient2(low = "deepskyblue4",
+                        mid = "antiquewhite2",
+                        high = "firebrick4", midpoint = 52) +
+  scale_size(name = "per capita FFLs", range = c(1, 42)) +
+  geom_text(aes(agriculture, manufacturing, label = .rownames),
+            size = 9.5, hjust = 0.9, vjust = -0.55, 
+            check_overlap = T, family = "GillSans", data = industry.perCapita) +
+  expand_limits(x = c(-50, 7000)) +
+  pd.hires +
+  theme(legend.position = "none",
+        legend.title = element_text(size = 10),
+        panel.grid = element_blank()) +
+  labs(x = "Agriculture, Hunting & Fishing, Mining workforce per 100k", 
+       y = "Manufacturing workforce per 100k",
+       color = "per capita FFLs")
+
+
+
